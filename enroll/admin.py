8ae31b9d5c15b1da376aa68c8d4c79655d2a7d4d
Himanshu Singh <tkdhimanshusingh@gmail.com>
2022-09-27 16:04:15 +0530

    Full table view using decorators

--- enroll/admin.py
+++ enroll/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from enroll.models import Student
 # Register your models here.
-
+@admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display=('id','stuid','stuname','stuemail')
-admin.site.register(Student,StudentAdmin)
+#admin.site.register(Student,StudentAdmin)
